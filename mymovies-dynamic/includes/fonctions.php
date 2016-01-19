eecc952928b60454d52da9ed156919925268ca02
Julien BONTRON <bontronjulien73@gmail.com>
2016-01-19 18:37:55 +0100

    Modifications

--- mymovies-dynamic/includes/fonctions.php
+++ mymovies-dynamic/includes/fonctions.php
@@ -1,6 +1,8 @@
 <?php
     function connectionbd()
     {
+        //Connexion MySQL 
+        //$bdd =new PDO('mysql:host=localhost;dbname=mymovies', 'mymovies_user', 'secret', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         // MySQL config for OpenShift
         $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
         $dbName = getenv('OPENSHIFT_GEAR_NAME');
@@ -19,26 +21,18 @@ function escape($valeur)
 
     function ajoutfilm($titre, $desc1, $desc2, $real, $annee, $img)
     {
-		$titre = str_replace("'", "\'",$titre);
-		$desc1 = str_replace("'", "\'",$desc1);
-		$desc2 = str_replace("'", "\'",$desc2);
-		$real = str_replace("'", "\'",$real);
-		$img = str_replace("'", "\'",$img);
         $stmtFilm = connectionbd()->prepare("INSERT INTO movie values ('', ? , ?, ?, ?, ?, ?);");
         $stmtFilm->execute(array($titre,$desc1,$desc2,$real,$annee,$img));
     }
 	
 	function editfilm($id, $titre, $desc1, $desc2, $real, $annee, $img)
     {
-
         $stmtFilmE = connectionbd()->prepare("UPDATE movie SET mov_title=?, mov_description_short=?, mov_description_long=?, mov_director=?, mov_year=?, mov_image=? WHERE mov_id=?;");
         $stmtFilmE->execute(array($titre,$desc1,$desc2,$real,$annee,$img,$id));
     }
 
 function ajoutMembre($login,$mdp)
 {
-    $login = str_replace("'", "\'",$login);
-    $mdp = str_replace("'", "\'",$mdp);
     $stmtMembre = connectionbd()->prepare("INSERT INTO membre values ('',?,?);");
     $stmtMembre->execute(array($login,$mdp));
 }
